bf6daaf82d797361346683d657dbf403c43ab261
Rashmi <rashmi.bhavsar02@gmail.com>
2025-01-08 17:05:59 +0100

    test order

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,18 @@
 module.exports = {
+    preset: 'ts-jest',
     transform: {
-      "^.+\\.tsx?$": "babel-jest", // Transpile TypeScript files
-      "^.+\\.js$": "babel-jest"    // Transpile JavaScript files
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+      '^.+\\.(js|jsx)$': 'babel-jest',
     },
+    testEnvironment: 'node',
     moduleFileExtensions: ['js', 'ts', 'tsx'],
+    transformIgnorePatterns: [
+      '/node_modules/(?!(@babel|your-module-name))',  // Ne pas ignorer certains modules dans node_modules
+    ],
+    globals: {
+      'ts-jest': {
+        useBabelrc: true, // Utilisation du fichier .babelrc pour la configuration
+      },
+    },
   };
   
